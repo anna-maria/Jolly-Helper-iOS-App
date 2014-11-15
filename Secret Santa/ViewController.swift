0f4a5fd1e9eb81e4ae0050153e7b8938df5a6215
Anna Gyergyai <anna@Macintosh-32.local>
2014-11-15 17:39:24 -0500

    Initial Commit

---
+++ Secret Santa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secret Santa
+//
+//  Created by Anna Gyergyai on 2014-11-15.
+//  Copyright (c) 2014 Anna Gyergyai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
